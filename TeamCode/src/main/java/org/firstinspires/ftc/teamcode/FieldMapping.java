461f73a7a67ab5b1f96d40b98de55855f803442e
Tony <tony.skeps@gmial.com>
2019-11-23 10:53:26 -0600

    Added "AddObstacle" method in FieldMapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMapping.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -7,6 +8,9 @@ import java.util.List;
 public class FieldMapping {
     public float length;
     public float width;
+
+    public ArrayList<FieldObstacle> obstacles = new ArrayList<>();
+
     public FieldMapping(float length, float width) {
         this.length = length;
         this.width = width;
@@ -15,6 +19,7 @@ public class FieldMapping {
 
     // width is the X dimension, length is the Y dimension
     public void addObstacle(float x, float y, float width, float length) {
+        obstacles.add(new FieldObstacle(x,y,length,width,0));
     }
 
 
