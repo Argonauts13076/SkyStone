20d3cc80bca8610bd08cf32b9a2d06fe09a341af
aidan <aidan@mallberg.com>
2019-11-23 14:18:12 -0600

    Changed references for angles Should work to move to predefined spot with some limitations
    TODO: catch Y Z rotation flip (aka heading jumps) and handle
    TODO: estimated distance without images, have images update in the loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaTest.java
@@ -3,9 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.*;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.RADIANS;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
 
@@ -29,19 +33,21 @@ public class VuforiaTest extends LinearOpMode {
 
         Position tempTarget = new Position(-48, 48, 0, 0);
         double[] tempResult;
+
         while (opModeIsActive()) {
             OpenGLMatrix fieldPos = hardware.fieldTracker.getPosVuf();
 
             if (fieldPos != null) {
-                Orientation rotation = Orientation.getOrientation(fieldPos, EXTRINSIC, XYZ, DEGREES);
+                Orientation rotation = Orientation.getOrientation(fieldPos, EXTRINSIC, AxesOrder.XYZ, RADIANS );
                 VectorF translation = fieldPos.getTranslation();
                 Position curPos;
                 telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
                         translation.get(0)/25.4, translation.get(1)/25.4, translation.get(2)/25.4);
-                telemetry.addData("Heading", "%.0f", rotation.thirdAngle);
+                telemetry.addData("Headinng", "%.2f, %.2f, %.2f", Math.toDegrees(rotation.thirdAngle - (Math.PI/2.0)),
+                                  Math.toDegrees(rotation.firstAngle), Math.toDegrees(rotation.secondAngle));
 
                 curPos = new Position(translation.get(0)/25.4, translation.get(1)/25.4,
-                        Math.toRadians(rotation.thirdAngle), 100);
+                                    rotation.thirdAngle - (Math.PI/2.0), 100);
                 tempResult = Robot.computeAngleAndPower(curPos, tempTarget);
                 telemetry.addData("Beta", "%.1f", Math.toDegrees(Math.atan2(tempTarget.Y - curPos.Y, tempTarget.X - curPos.X)));
                 telemetry.addData("Angle and power", "%.1f, %.1f", Math.toDegrees(tempResult[0]), tempResult[1]);
@@ -56,10 +62,10 @@ public class VuforiaTest extends LinearOpMode {
                     tempPower = 1.0;
                 }
 
-                double frontRight = (-((cos + sin) * tempResult[1]));
-                double frontLeft = ((cos - sin) * tempResult[1]);
-                double rearLeft = ((cos + sin) * tempResult[1]);
-                double rearRight = (-((cos - sin) * tempResult[1]));
+                double frontRight = (-((cos - sin) * tempResult[1]));
+                double frontLeft = ((cos + sin) * tempResult[1]);
+                double rearLeft = ((cos - sin) * tempResult[1]);
+                double rearRight = (-((cos + sin) * tempResult[1]));
 
                 telemetry.addData("FR FL RL RR Power", "%.1f, %.1f, %.1f, %.1f", frontRight, frontLeft,
                         rearLeft, rearRight);
@@ -71,8 +77,12 @@ public class VuforiaTest extends LinearOpMode {
             sleep(500);
         }
 
+
 /*
+
     robot.moveAngle(0, 0.3);
+    sleep(1000);
+    robot.moveAngle(Math.PI/4, 0.3);
     sleep(1000);
     robot.moveAngle(Math.PI/2, 0.3);
     sleep(1000);
