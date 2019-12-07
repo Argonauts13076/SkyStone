0924b584e2dc3f6fe67993b4c359f68cfeb24de9
Tony <tony.skeps@gmial.com>
2019-12-07 13:20:18 -0600

    implemented error handling BasicDriveTrainTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveTrainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveTrainTeleOp.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.*;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name="Test: Basic Drive Train", group="Test")
 public class BasicDriveTrainTeleOp extends LinearOpMode {
 
@@ -16,7 +18,9 @@ public class BasicDriveTrainTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        hardware.init(hardwareMap);
+        String s = hardware.init(hardwareMap);
+
+        telemetry.addData("Errors", s);
 
         waitForStart();
 
@@ -35,78 +39,84 @@ public class BasicDriveTrainTeleOp extends LinearOpMode {
             float gamepad2RightTrigger = gamepad2.left_trigger;
             boolean gamepad2A = gamepad2.a;
 
-            if(gamepad1X){
-                speedDivisor = 4;
-            }
-            if(gamepad1A){
-                speedDivisor = 2;
-            }
-            if(gamepad1B) {
-                speedDivisor = 1;
-            }
-
-            float FrontLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
-            float FrontRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
-            float BackLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
-            float BackRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
-
-            // clip the right/left values so that the values never exceed +/- 1
-            FrontLeft = Range.clip(FrontLeft, -1, 1);
-            FrontRight = Range.clip(FrontRight, -1, 1);
-            BackLeft = Range.clip(BackLeft, -1, 1);
-            BackRight = Range.clip(BackRight, -1, 1);
-
-            // write the values to the motors
-            hardware.FrontLeft.setPower(FrontLeft/speedDivisor);
-            hardware.FrontRight.setPower(FrontRight/speedDivisor);
-            hardware.RearLeft.setPower(BackLeft/speedDivisor);
-            hardware.RearRight.setPower(BackRight/speedDivisor);
-
-            if(gamepad2LeftTrigger > 0.5 && gamepad2RightTrigger > 0.5){
-                hardware.scissorLiftManualOverride();
-            }else{
-                hardware.revokeScissorLiftManualOverride();
-            }
-
-            if(hardware.getManualOverride()){
-                if(!(gamepad2DPadUp == gamepad2DPadDown)){
-                    if (gamepad2DPadUp) {
-                        hardware.ScissorLift.setPower(BasicDriveTrainHardware.ScissorLiftPower);
-                    }
-                    if (gamepad2DPadDown) {
-                        hardware.ScissorLift.setPower(-BasicDriveTrainHardware.ScissorLiftPower);
-                    }
-                }else{
-                    hardware.ScissorLift.setPower(0);
+            if(hardware.canUseWheels) {
+                if (gamepad1X) {
+                    speedDivisor = 4;
                 }
-            }else{
-                if(!(gamepad2DPadUp == gamepad2DPadDown)){
-                    if(scissorLiftLatch) {
+                if (gamepad1A) {
+                    speedDivisor = 2;
+                }
+                if (gamepad1B) {
+                    speedDivisor = 1;
+                }
+
+                float FrontLeft = -gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+                float FrontRight = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+                float BackLeft = -gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+                float BackRight = gamepad1LeftY - gamepad1LeftX - gamepad1RightX;
+
+                // clip the right/left values so that the values never exceed +/- 1
+                FrontLeft = Range.clip(FrontLeft, -1, 1);
+                FrontRight = Range.clip(FrontRight, -1, 1);
+                BackLeft = Range.clip(BackLeft, -1, 1);
+                BackRight = Range.clip(BackRight, -1, 1);
+
+                // write the values to the motors
+                hardware.FrontLeft.setPower(FrontLeft / speedDivisor);
+                hardware.FrontRight.setPower(FrontRight / speedDivisor);
+                hardware.RearLeft.setPower(BackLeft / speedDivisor);
+                hardware.RearRight.setPower(BackRight / speedDivisor);
+            }
+
+            if(hardware.canUseScissorLift) {
+                if (gamepad2LeftTrigger > 0.5 && gamepad2RightTrigger > 0.5) {
+                    hardware.scissorLiftManualOverride();
+                } else {
+                    hardware.revokeScissorLiftManualOverride();
+                }
+
+                if (hardware.getManualOverride()) {
+                    if (!(gamepad2DPadUp == gamepad2DPadDown)) {
                         if (gamepad2DPadUp) {
-                            hardware.incrementPosition();
-                            scissorLiftLatch = false;
+                            hardware.ScissorLift.setPower(BasicDriveTrainHardware.ScissorLiftPower);
                         }
                         if (gamepad2DPadDown) {
-                            hardware.decrementPosition();
-                            scissorLiftLatch = false;
+                            hardware.ScissorLift.setPower(-BasicDriveTrainHardware.ScissorLiftPower);
                         }
+                    } else {
+                        hardware.ScissorLift.setPower(0);
                     }
-                }else{
-                    scissorLiftLatch = true;
+                } else {
+                    if (!(gamepad2DPadUp == gamepad2DPadDown)) {
+                        if (scissorLiftLatch) {
+                            if (gamepad2DPadUp) {
+                                hardware.incrementPosition();
+                                scissorLiftLatch = false;
+                            }
+                            if (gamepad2DPadDown) {
+                                hardware.decrementPosition();
+                                scissorLiftLatch = false;
+                            }
+                        }
+                    } else {
+                        scissorLiftLatch = true;
+                    }
                 }
             }
 
-            if(gamepad2A){
-                if(gripperLatch){
-                    gripperLatch = false;
-                    if(hardware.getGripperState()){
-                        hardware.CloseGripper();
-                    }else{
-                        hardware.OpenGripper();
+            if(hardware.canUseGripper) {
+                if (gamepad2A) {
+                    if (gripperLatch) {
+                        gripperLatch = false;
+                        if (hardware.getGripperState()) {
+                            hardware.CloseGripper();
+                        } else {
+                            hardware.OpenGripper();
+                        }
                     }
+                } else {
+                    gripperLatch = true;
                 }
-            }else{
-                gripperLatch = true;
             }
 
             sleep(50);
