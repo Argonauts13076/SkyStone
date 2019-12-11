d0606848531a4bd907f1d6c3049b544e28acf5a2
Tony <tony.skeps@gmial.com>
2019-12-10 21:21:36 -0600

    change of values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveTrainHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriveTrainHardware.java
@@ -111,12 +111,12 @@ public class BasicDriveTrainHardware {
     // ToDo Set Values
     public static int[] ScissorLiftPositionList = new int[]{0,10,100};
     public static double ScissorLiftPower = 0.5;
-    public static double GripperLeftDefaultPosition = 0.165;
+    public static double GripperLeftDefaultPosition = 0.705;
     public static double GripperLeftClosePosition = 0.75;
     public static double GripperLeftOpenPosition = 0.25;
-    public static double GripperRightDefaultPosition = 0.705;
+    public static double GripperRightDefaultPosition = 0.165;
     public static double GripperRightClosePosition = 0.75;
-    public static double GripperRightOpenPosition = 0.25;
+    public static double GripperRightOpenPosition = 0.705;
 
     private int currentPosition = 0;
     private boolean gripperState = false;
